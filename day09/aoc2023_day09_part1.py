37082bdca70e4c31ce88090827a017dae1e921b8
supvolume <h.supida@gmail.com>
2023-12-28 21:28:54 +0700

    AOC 2023 day9

---
+++ day09/aoc2023_day09_part1.py
@@ -0,0 +1,25 @@
+# AOC 2023 day 9 part 1
+
+txt = open("input_day09.txt", 'r').read()
+txt_list = list(txt.split("\n"))
+
+history_list = []
+for i in txt_list:
+    history_list.append(list(int(j) for j in i.split(" ")))
+
+#Recursion until the final list contain 0 only
+def prediction(value_list):
+    if all(i==0 for i in value_list):
+        return 0
+    else:
+        temp_list = []
+        for i in range(len(value_list)-1):
+            temp_list.append(value_list[i+1]-value_list[i])        
+        return value_list[-1] + prediction(temp_list)
+    
+all_predict = []
+for history in history_list:
+    all_predict.append(prediction(history))
+
+print(sum(all_predict))
+
